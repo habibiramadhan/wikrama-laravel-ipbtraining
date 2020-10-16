999e74fe42a5e44a50978d40245cde63c6ea4e4f
SyahalTastari <55316168+SyahalTastari@users.noreply.github.com>
2020-10-16 18:26:04 +0700

    menambahkan inputan sederhana

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ParticipantController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ParticipantController::class, 'index']);
+Route::post('/participant/kuesioner', [ParticipantController::class, 'create']);
+Route::get('/ajax-name',function(Request $request){
+	$title = $request->title;
+	
+	$sub = DB::table('product_participant')
+      ->join('products', 'product_participant.product_id', '=', 'products.id')
+      ->join('participants', 'product_participant.participant_id', '=', 'participants.id')
+      ->where('product_participant.product_id', '=', $title)
+      ->select('participants.id', 'participants.name')
+      ->get();
+	return Response::json($sub);
 });
